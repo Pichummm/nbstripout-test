0a39b1ee7668023a5d659ed13f79c94dd9f08a1f
Pichum <iritaryo@gmail.com>
2024-10-03 06:52:16 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import random
+
+def print_random_numbers(count, start, end):
+    for _ in range(count):
+        random_number = random.randint(start, end)
+        print(random_number)
+
+# 使用例: 5つのランダムな整数を1から10の範囲で生成
+print_random_numbers(5, 1, 10)
+
