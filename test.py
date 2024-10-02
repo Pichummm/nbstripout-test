3b00213ba79523a2b191692f61606eec8d0ee234
Pichum <iritaryo@gmail.com>
2024-10-03 07:01:59 +0900

    third commit

--- test.py
+++ test.py
@@ -14,3 +14,17 @@ def print_random_numbers(count, start, end):
 # 使用例: 5つのランダムな整数を1から10の範囲で生成
 print_random_numbers(5, 1, 10)
 
+
+# In[2]:
+
+
+import random
+
+def print_random_numbers(count, start, end):
+    for _ in range(count):
+        random_number = random.randint(start, end)
+        print(random_number)
+
+# 使用例: 5つのランダムな整数を1から10の範囲で生成
+print_random_numbers(5, 1, 20)
+
